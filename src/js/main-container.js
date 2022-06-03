a55d0d74341deb8b1fa3b3be1fc6aaeb4197d35c
Dominik <dominik.merdzik@onet.pl>
2022-06-03 22:17:41 +0200

    test

--- src/js/main-container.js
+++ src/js/main-container.js
@@ -3,10 +3,14 @@ template.innerHTML = `
     <link rel="stylesheet" type="text/css" href="src/scss/main.css">
     <section class="main-module">
         <div class="main-module-image__container">
-            <img class="displayedPicture"/>
+            <img class="displayedPicture" alt="picture representing the beach and the sea on the right side"/>
         </div>
         <div class="main-module-content__container">
-        
+            <div class="content">
+                <h1>Lorem Ipsum</h1>
+                <p>Infinitely scalable, feature-rich and cloud-native data management and protection for modern and legacy infrastructures amd SaaS platforms, managed via a single app with no hardware required</p>
+            </div>
+            <button>Button</button>
         </div>
     </section>
 `;
